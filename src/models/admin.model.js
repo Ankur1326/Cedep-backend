b7f397d26158dc8a9ee2813a4e7c397946180b37
Ankur1326 <swamiankur46@gmail.com>
2024-07-17 17:24:52 +0530

    create modal for admin

---
+++ src/models/admin.model.js
@@ -0,0 +1,81 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const adminSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true,
+            index: true
+        },
+        fullName: {
+            type: String,
+            unique: true,
+            lowecase: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true,
+        },
+        mobNumber: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        confirmPassword: {
+            type: String,
+        },
+    }
+)
+
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+adminSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+adminSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        // {
+        //     expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        // }
+    )
+}
+
+adminSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        // {
+        //     expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        // }
+    )
+}
+
+export const Admin = mongoose.model("Admin", adminSchema)
